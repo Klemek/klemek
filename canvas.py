bdd6eecd93d536ffe4022cce3a03f6fe0156eab7
Klemek <klemek@outlook.fr>
2021-12-13 13:58:32 +0100

    better thread handling

--- canvas.py
+++ canvas.py
@@ -15,9 +15,14 @@ REF_URL = 'https://raw.githubusercontent.com/Klemek/klemek/main/ref.png'
 URL = 'http://challs.xmas.htsp.ro:3002/'
 W = 320
 H = 240
+TIMEOUT = 10
 
-cached_targets = []
-lock = Lock() 
+class Data:
+    def __init__(self) -> None:
+        self.cached_targets = []
+        self.lock = Lock()
+        self.t0 = None
+
 
 def convert24to16(r, g, b):
     return (r >> 3, g >> 3, b >> 3)
@@ -26,40 +31,37 @@ def diff(ref, col):
     ref, col = convert24to16(*ref), convert24to16(*col)
     return sum(abs(ref[i] - col[i]) for i in range(3))
 
-def get_next_pixel():
-    global cached_targets
+def get_next_pixel(data):
     headers = {
         "Cache-Control": "no-cache",
         "Pragma": "no-cache"
     }
     r = requests.get(f"{REF_URL}", headers=headers)
     ref_img = Image.open(BytesIO(r.content))
-    r = requests.get(f"{URL}/canvas.png", headers=headers)
-    lock.acquire()
-    if r.status_code != 200:
-        print(f"cannot get ref image: {r.status_code} {r.reason}")
-    else:
-        with open(f"{dir}/dist.png", mode='wb') as f:
-            f.write(r.content)
-    lock.release()
-    targets = cached_targets
     total = sum(1 if ref_img.getpixel((x, y))[3] > 0 else 0 for x in range(W) for y in range(H))
-    try:
-        dist_img = Image.open(f"{dir}/dist.png").convert(mode='RGB')
-        targets = []
-        for x in range(W):
-            for y in range(H):
-                ref_color = ref_img.getpixel((x, y))
-                dist_color = dist_img.getpixel((x * 2, y * 2))
-                if ref_color[3] > 0 and diff(ref_color[:3], dist_color) > 5:
-                    targets += [(x, y, ref_color[:3], dist_color)]
-    except OSError:
-        print("using cached targets")
+    data.lock.acquire()
+    targets = data.cached_targets[::1]
+    if data.t0 is None or (time.time() - data.t0) > TIMEOUT:
+        r = requests.get(f"{URL}/canvas.png", headers=headers)
+        if r.status_code == 200:
+            try:
+                data.t0 = time.time()
+                dist_img = Image.open(BytesIO(r.content)).convert(mode='RGB')
+                targets = []
+                for x in range(W):
+                    for y in range(H):
+                        ref_color = ref_img.getpixel((x, y))
+                        dist_color = dist_img.getpixel((x * 2, y * 2))
+                        if ref_color[3] > 0 and diff(ref_color[:3], dist_color) > 5:
+                            targets += [(x, y, ref_color[:3], dist_color)]
+                data.cached_targets = targets
+            except OSError:
+                pass
+    data.lock.release()
     if len(targets) == 0:
         print("no pixel to update")
         return None
     else:
-        cached_targets = targets
         x, y, ref_color, dist_color = random.choice(targets)
         print(f"pixel {x},{y} is {dist_color} and should be {ref_color} (1 of {len(targets)}/{total})({(total-len(targets))/total:.2%})")
         return x, y, f"{ref_color[0]:02x}{ref_color[1]:02x}{ref_color[2]:02x}".upper()
@@ -123,14 +125,14 @@ def paint(pixel_data, challenge, answer):
         print(f"cannot update pixel: {r.status_code} {r.reason}")
 
 
-def work():
+def work(data):
     while True:
         try:
             challenge = get_challenge()
             if challenge is None:
                 continue
             answer = solve_challenge(challenge)
-            pixel_data = get_next_pixel()
+            pixel_data = get_next_pixel(data)
             if pixel_data is None:
                 continue
             paint(pixel_data, challenge, answer)
@@ -141,6 +143,7 @@ def work():
 
 
 if __name__ == '__main__':
+    data = Data()
     n_threads = int(os.environ['N']) if 'N' in os.environ else 8
     for p in range(n_threads):
-        Process(target=work).start()
+        Process(target=work, args=(data,)).start()
